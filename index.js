cc1177146c10f6dc133085759b8bb8c5ab12d52e
tonmoy <tonmoy3673@gmail.com>
2024-01-23 21:45:09 +0600

    run the server

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const app=express()
 const cors=require('cors');
 const port=5000;
 app.use(cors());
+app.get('/',(req,res)=>{
+    res.send('House Hunter Service is Running')
+})
+
+app.listen(port,()=>{
+    console.log(`House Hunter is running on ${port}`)
+})
