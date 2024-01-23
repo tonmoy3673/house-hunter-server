8029680296d6bf8602f1e0c17719dcfae01472ca
tonmoy <tonmoy3673@gmail.com>
2024-01-24 00:27:50 +0600

    implemented update

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ async function run() {
     try {
 
         const houseCollection = client.db('HouseHunter').collection('houses');
-        
-
-
-
+    
 
         app.get('/houses', async (req, res) => {
             const query = {};
@@ -59,7 +56,34 @@ async function run() {
             res.send(house)
         })
 
+        app.get('/house/:id',async(req,res)=>{
+            try {
+                const id = req.params.id;
+                const query = { _id: new ObjectId(id) };
+            
+                const house = await houseCollection.findOne(query);
+            
+                if (!house) {
+                  return res.status(404).json({ error: 'House not found' });
+                }
+                res.json(house);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: 'Internal Server Error' });
+            }
+        })
 
+        app.put('/houses/:id', async (req, res) => {
+            const house = req.body;
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            
+            const updateDoc = {
+                $set: house
+            }
+            await houseCollection.updateOne(filter, updateDoc)
+            res.status(201).send({ massage: 'Update successfully' });
+        })
 
         console.log('Connected to MongoDB!');
     } catch (error) {
