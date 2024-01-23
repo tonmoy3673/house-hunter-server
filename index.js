2924e8f30f6c9c3039866ced6f93516b5156d561
tonmoy <tonmoy3673@gmail.com>
2024-01-23 23:29:10 +0600

    set mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express=require('express');
 const app=express()
 const cors=require('cors');
-const port=5000;
+const bcrypt=require('bcrypt');
+const bodyParser=require('body-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const port=process.env.PORT || 5000;
 app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json())
 app.get('/',(req,res)=>{
     res.send('House Hunter Service is Running')
 })
